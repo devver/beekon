92747e8061793be26c90173791167850a3c3db40
Ben Brinckerhoff <ben@devver.net>
2009-02-17 22:39:31 -0700

    Created file to hold daily activity, which can later be used to computer averages

--- beekon.rb
+++ beekon.rb
@@ -4,9 +4,14 @@ require 'open-uri'
 require 'yaml'
 
 @config = YAML::load_file('config.yml')
+@activity = {}
 
 def api_token
   @config['API_TOKEN']
+end
+
+def activity_file
+  @config['ACTIVITY_FILE']
 end
 
 def project_url
@@ -20,21 +25,51 @@ end
 def tickets_created_on(date)
   url = tickets_url+"&q=created:'#{date}\'"
   document = Hpricot(open(url))
-  tickets_numbers = (document/"/tickets/ticket/number").map{|x| Integer(x.inner_html)}
+  tickets(document)
 end
 
 def tickets_closed_on(date)
   url = tickets_url+"&q=updated:'#{date}'+state:closed"
   document = Hpricot(open(url))
-  tickets_numbers = (document/"/tickets/ticket/number").map{|x| Integer(x.inner_html)}
+  tickets(document)
+end
+
+def tickets(document)
+  (document/"/tickets/ticket").map{|x| {:milestone => Integer(x.at("milestone-id").inner_html), :number => Integer(x.at("number").inner_html)}}
 end
 
 def velocity_for(date)
-  velocity = tickets_closed_on(date).length-tickets_created_on(date).length
+  tickets_closed_on(date).length-tickets_created_on(date).length
 end
 
-puts velocity_for("2/17/2009")
-puts "done"
+def activity_for(date)
+  {:closed => tickets_closed_on(date),
+    :created => tickets_created_on(date) }
+end
+
+def load_activity
+  if(File.exists?(activity_file))
+    @activity = YAML::load_file(activity_file)
+  else
+    @activity = {}
+  end
+end
+
+def save_activity
+  File.open(activity_file,'w') do |f|
+    f << @activity.to_yaml
+  end
+end
+
+def add_activity_for(date)
+  @activity[date] = activity_for(date)
+end
+
+load_activity
+add_activity_for("2/17/2009")
+save_activity
+
+puts @activity.inspect
 
 
 
