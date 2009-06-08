7b0a69e71207321e6c1db68809a937773328dfac
Ben Brinckerhoff <ben@devver.net>
2009-06-08 00:44:50 -0600

    Fixed the finish_milestone.rb script to move all tickets by using the 'page' option

--- finish_milestone.rb
+++ finish_milestone.rb
@@ -1,21 +1,19 @@
 #!/usr/bin/env ruby
-#
-# Script to set all "fixed" Lighthouse tickets to deployed
- 
+
 require 'ruby-debug'
 require 'lighthouse-api'
 require 'yaml'
- 
+
 CONFIG_FILE = 'finish_milestone.yml'
 config = YAML.load_file(CONFIG_FILE)
 
 current_milestone_title = 'Publicity Launch'
 new_milestone_title = 'closed beta'
 
-Lighthouse.account = config['account'] # put your account name in here
-Lighthouse.token = config['api-token'] # put your API token here
+Lighthouse.account = config['account'] 
+Lighthouse.token = config['api-token'] 
 
-project = Lighthouse::Project.find(config['project-id']) # replace with your project's ID
+project = Lighthouse::Project.find(config['project-id']) 
 
 milestones = project.milestones
 current_milestone = milestones.select {|milestone| milestone.title==current_milestone_title}.first
@@ -30,13 +28,9 @@ if(new_milestone==nil)
   exit 1
 end
 
-def tickets_to_move(project,milestone)
-  # I think it's more efficient to do this like so:
-  ## (for some reason, searching for 'open' tickets returns both 'open' and 'new' tickets)
-  tickets = project.tickets(:q => "milestone_id:'#{milestone.id}' state:open ")
-  # but it seems that lighthouse caches the results for queries like this, so I can't 
-  # go through more than 30 tickets
-  #tickets = milestone.tickets.select{|ticket| ticket.state=='open' || ticket.state=='new'}
+def tickets_to_move(project,milestone,page=1)
+  # for some reason, searching for 'open' tickets returns both 'open' and 'new' tickets
+  tickets = project.tickets(:q => "milestone_id:'#{milestone.id}' state:open", :page => page)
   if(tickets.empty?)
     puts "No more tickets in '#{milestone.title}' to move"
   else
@@ -45,7 +39,8 @@ def tickets_to_move(project,milestone)
   tickets
 end
 
-tickets = tickets_to_move(project,current_milestone)
+page = 1
+tickets = tickets_to_move(project,current_milestone,page)
 # You get a hard limit of 30 tickets on each query, so we have to do this repeatedly until nothing is returned
 while(tickets!=[]) do
   tickets.each do |ticket|
@@ -57,12 +52,9 @@ while(tickets!=[]) do
     ticket.save
     puts "done."
   end
-  tickets = tickets_to_move(project,current_milestone)
+  page += 1
+  tickets = tickets_to_move(project,current_milestone,page)
 end
 
-#ticket = tickets_to_move(current_milestone).first 
-#puts ticket.id
-#ticket.milestone_id=new_milestone.id
-#ticket.save
 
 
